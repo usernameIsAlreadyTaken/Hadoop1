963b5542ff6e58cd41fcd6a963940fdf0b6b7484
hadoop <hadoop@hadoop1.(none)>
2014-03-14 15:13:48 +0800

    add wordcount

---
+++ src/org/hadoop/wordcount/WCMapper.java
@@ -0,0 +1,26 @@
+package org.hadoop.wordcount;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WCMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
+	
+	@Override
+	protected void map(Object key, Text value, Context context)
+			throws IOException, InterruptedException {
+		StringTokenizer st = new StringTokenizer(value.toString());
+		while(st.hasMoreTokens()){
+			word.set(st.nextToken());
+			context.write(word, one);
+		}
+		
+	}
+	
+}
